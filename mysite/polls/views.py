d728a79db01a3ebf8ada865b386840e8444e73ec
Van Cleave, Aaron <avancleave@samaritan.org>
2021-07-21 14:49:18 -0400

    added detail template

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,14 +1,24 @@
-# from django.shortcuts import render
+from django.shortcuts import render
 
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.template import Context, loader
 
+from .models import Question
+
 def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
     template = loader.get_template("polls/index.html")
-    return HttpResponse(template.render())
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
 
 def detail(request, question_id):
-    return HttpResponse("You are looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
 
 def vote(request, question_id):
     return HttpResponse("You are voting on question %s." % question_id)
