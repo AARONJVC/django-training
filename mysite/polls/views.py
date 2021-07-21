99f388f4fd5adb4b5550585a844fcc4e1fab08a1
Van Cleave, Aaron <avancleave@samaritan.org>
2021-07-21 14:04:05 -0400

    added urlconf paths that accept integer inputs

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,3 +4,12 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, this is the polls index!")
+
+def detail(request, question_id):
+    return HttpResponse("You are looking at question %s." % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You are voting on question %s." % question_id)
+
+def results(request, question_id):
+    return HttpResponse("You are looking at the results of question %s." % question_id)
