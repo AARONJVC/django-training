effbbbbb1202f3ce4cf94ca0cda516020d1435ba
Van Cleave, Aaron <avancleave@samaritan.org>
2021-09-29 15:36:44 -0400

    initial tutorial files

---
+++ meeting_planner/website/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from datetime import datetime
+
+# Create your views here.
+
+def welcome(request):
+        return  HttpResponse("Welcome to the thing")
+
+def date(request):
+        return  HttpResponse("Page served at " + str(datetime.now()))
+
