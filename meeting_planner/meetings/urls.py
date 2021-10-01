3231cf23086e58a954abdcbf5826143e89e350e2
Van Cleave, Aaron <avancleave@samaritan.org>
2021-10-01 15:32:30 -0400

    added post methods for creating rooms and meetings

--- meeting_planner/meetings/urls.py
+++ meeting_planner/meetings/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('<int:id>', views.detail, name="detail"),
     path('rooms', views.rooms, name="rooms"),
+    path('new-meeting', views.new_meeting, name="new meeting"),
+    path('new-room', views.new_room, name="new room"),
 ]
--- meeting_planner/meetings/views.py
+++ meeting_planner/meetings/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.forms import modelform_factory
 
 from .models import Meeting, Room
 
@@ -9,5 +10,28 @@ def detail(request, id):
     return render(request, "meetings/detail.html", {"meeting": meeting})
 
 def rooms(request):
-    return  render(request, "rooms/rooms.html", {"rooms": Room.objects.all(), "count": Room.objects.count()})
+    return  render(request, "meetings/rooms.html", {"rooms": Room.objects.all(), "count": Room.objects.count()})
 
+MeetingForm = modelform_factory(Meeting, exclude=[])
+
+def new_meeting(request):
+    if request.method == "POST":
+        form = MeetingForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("welcome")
+    else:
+        form = MeetingForm()
+    return render(request, "meetings/new-meeting.html", {"form": form})
+
+RoomForm = modelform_factory(Room, exclude=[])
+
+def new_room(request):
+    if request.method == "POST":
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("rooms")
+    else:
+        form = RoomForm()
+    return render(request, "meetings/new-room.html", {"form": form})
